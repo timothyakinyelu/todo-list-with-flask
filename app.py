d3a8c1e2fa2cd2bbfbdf5fb3747283983dfb6ff9
loverofMush <tolujamal1@gmail.com>
2020-09-29 00:42:04 +0100

    - created a database connection - fetch all tasks and render in template

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 @app.route("/")
 def index():
-    return '<h1>Todo List App<h1>'
+    conn = get_db_connection()
+    tasks = conn.execute('SELECT * FROM tasks').fetchall()
+    conn.close()
+    return render_template('index.html', tasks=tasks)
+
+@app.route("/todo-detail")
+def detail():
+    return render_template('todo-detail.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
