908938bf5ce79460626fddcd1029d64b53b9bc65
loverofMush <tolujamal1@gmail.com>
2020-09-30 00:02:17 +0100

    - insert tasks into index.html - createTask function added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from itertools import groupby
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -13,6 +13,7 @@ app.static_folder = 'static'
 @app.route("/")
 def index():
     conn = get_db_connection()
+    task_types = conn.execute('SELECT * FROM task_types').fetchall()
     tasks = conn.execute(
         'SELECT i.*, t.name FROM tasks i JOIN task_types t ON i.task_type_id = t.id ORDER BY t.name'
     ).fetchall()
@@ -22,11 +23,34 @@ def index():
         lists[k] = list(g)  
      
     conn.close()
-    return render_template('index.html', lists=lists)
+    return render_template('index.html', lists=lists, types=task_types)
 
 @app.route("/todo-detail")
 def detail():
     return render_template('todo-detail.html')
+
+@app.route("/create-task", methods=('POST',))
+def createTask():
+    if request.method == 'POST':
+        task_type_id = request.form['task_type_id']
+        title = request.form['title']
+        content = request.form['content']
+        priority = request.form['priority']
+        
+        if not task_type_id:
+            flash('Task Type is required')
+        elif not title:
+            flash('Title is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO tasks (task_type_id, title, content, priority) VALUES(?, ?, ?, ?)', (task_type_id, title, content, priority))
+            
+            conn.commit()
+            conn.close()
+            
+            return redirect(url_for('index'))
+    # return render_template('create.html', types=task_types)
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
